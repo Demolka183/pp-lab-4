163e44fb46933ea3b21762c3caa428a20d3e8838
Demolka183 <166244786+Demolka183@users.noreply.github.com>
2024-04-14 12:16:05 +0200

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,23 @@
+import employees.*;
+
+public class Company {
+    public void run() {
+        Employee[] employees = new Employee[5];
+
+        employees[0] = new Manager("Wiktor Daniel", 5000);
+        employees[1] = new Worker("Jan Kowalski", 3500, "Programista");
+        employees[2] = new Worker("Piotr Nowak", 4000, "Projektant");
+        employees[3] = new Worker("Donald Tusk", 4200, "Inżynier");
+        employees[4] = new Worker("Jarosław Kaczyński", 3800, "Analityk");
+
+        System.out.println("Dane wszystkich pracowników:");
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    public static void main(String[] args) {
+        Company company = new Company();
+        company.run();
+    }
+}
